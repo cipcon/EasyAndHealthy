9d2d9030db5dc873da70e8ab456e0bd5263c3dfb
Ciprian Constantin <Ciprian.Constantin.ext@steep.de>
2024-06-10 15:47:19 +0200

    Integrating TypeScript

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,21 +1,33 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import Ingredients from './components/Ingredients';
+import Units from './components/Units'
 
 function App() {
-  const [ingredients, setIngredients] = useState([])
+  const [ingredient, setIngredient] = useState([]);
+  const [unit, setUnit] = useState([]);
 
   useEffect(() => {
     fetch('/ingredients/getAllIngredients')
       .then(res => res.json())
-      .then((data) => setIngredients(data))
+      .then((data) => setIngredient(data))
       .catch(console.error);
-  }, [])
+  }, []);
 
-    return (
-      <Ingredients ingredients={ingredients} />
-    );
-  
+  useEffect(() => {
+    fetch('/ingredients/getAllUnits')
+      .then(res => res.json())
+      .then((data) => setUnit(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <>
+      <Ingredients ingredients={ingredient} />
+      <Units units={unit} />
+    </>
+  );
+
 }
 
 export default App;
